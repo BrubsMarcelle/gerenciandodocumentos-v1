cd05416a206010f03fd77560306b3e5fd1cd5e97
BrunaMarcelle <bruna.silva@vocedm.com.br>
2024-03-04 10:46:52 -0300

    [01] Inclusao do logo ficticio e acoes do sub menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,27 @@
 import React,{FC } from 'react';
-import { Menu, Input, Button } from 'antd';
-import {LogoutOutlined, FilePdfOutlined} from '@ant-design/icons';
+import { Menu, Input, Typography } from 'antd';
+import {LogoutOutlined} from '@ant-design/icons';
+import logo from '../assets/logo.svg';
 
 const HeaderApp: FC =() =>{
     const onSair = () => (
         console.log("Você saiu xD")
     );
     return (
-        <Menu mode="horizontal" style={{ backgroundColor: '#1628ca'}} >
+        <Menu mode="horizontal" style={{ backgroundColor: '#1628ca', justifyContent: 'flex-start', alignItems: 'center'}} >
             <Menu.Item key="logo">
-                <FilePdfOutlined />
+                <img src={logo} alt='Imagem do logo de Gerenciador de Documentos' style={{ paddingTop: '5px' }}/>
             </Menu.Item>
             <Menu.Item key="search" >
-                <Input.Search placeholder="Pesquisar..." style={{ width: 200 }} />
+                <Input.Search placeholder="Pesquisar..." style={{ paddingTop: '5px' }} />
             </Menu.Item>
-            <Menu.Item key="profile" style={{flex: 100, justifyContent: "flex-end"}}>
-                <Button icon={<LogoutOutlined/>} onClick={onSair}>Sair</Button>
+            <Menu.Item  key="profile" style={{ marginLeft: 'auto' }}>
+                <Menu.SubMenu title={'Perfil'}>
+                    <Typography.Text>Usuario</Typography.Text>
+                    <Menu.Item key="configuracoes">Configurações</Menu.Item>
+                    <Menu.Item key="solicitar-acesso">Solicitar acesso</Menu.Item>
+                    <Menu.Item key="sair" icon={<LogoutOutlined/>} onClick={onSair}>Sair</Menu.Item>
+                </Menu.SubMenu>
             </Menu.Item>
         </Menu>
     );
