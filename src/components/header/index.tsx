2788c8c3e37776d28bc816cd07e5cb2944fd7bfe
BrunaMarcelle <bruna.silva@vocedm.com.br>
2024-03-07 08:53:15 -0300

    [01] Implementacao de thema dark ou padrao ao header.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,26 @@
-import React,{FC } from 'react';
+import React,{FC, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Menu, Input, Typography } from 'antd';
-import {LogoutOutlined} from '@ant-design/icons';
+import {LogoutOutlined, BulbOutlined, MoonOutlined} from '@ant-design/icons';
 import logo from '../assets/logo.svg';
 
 const HeaderApp: FC =() =>{
+    const [tema, setTema] = useState<string> ('light');
+
     const onSair = () => (
         console.log("Você saiu xD")
     );
+    const onMudarTheme = () =>
+    {
+        const novoTema = tema === 'light' ? 'dark' : 'light'; 
+        setTema(novoTema);
+        console.log('Você tentou mudar o theme da aplicação')
+    };
+
+    const usuario = 'Bruna Marcelle'
+    const iconTema = tema === 'light' ? <BulbOutlined /> : <MoonOutlined />;
     return (
-        <Menu mode="horizontal" style={{ backgroundColor: '#1628ca', justifyContent: 'flex-start', alignItems: 'center'}} >
+        <Menu mode="horizontal" style={{ backgroundColor: tema === 'light' ? '#1628ca' : '#000', justifyContent: 'flex-start', alignItems: 'center', color: tema === 'light' ? '#000' : '#fff'}} >
             <Menu.Item key="logo">
                 <img src={logo} alt='Imagem do logo de Gerenciador de Documentos' style={{ paddingTop: '5px' }}/>
             </Menu.Item>
@@ -16,10 +28,15 @@ const HeaderApp: FC =() =>{
                 <Input.Search placeholder="Pesquisar..." style={{ paddingTop: '5px' }} />
             </Menu.Item>
             <Menu.Item  key="profile" style={{ marginLeft: 'auto' }}>
-                <Menu.SubMenu title={'Perfil'}>
-                    <Typography.Text>Usuario</Typography.Text>
-                    <Menu.Item key="configuracoes">Configurações</Menu.Item>
-                    <Menu.Item key="solicitar-acesso">Solicitar acesso</Menu.Item>
+                <Menu.SubMenu title={<span style={{ color: '#FFFFFF' }}>{'Perfil'}</span>} style={{ display: 'flex', justifyContent: 'center' }} >
+                    <Typography.Text style={{ textAlign: 'center' }}>{usuario}</Typography.Text>
+                    <Menu.Item key="configuracoes">
+                        <Link to="/configuracoes">Configurações</Link>
+                    </Menu.Item>
+                    <Menu.Item key="solicitar-acesso">
+                        <Link to="/solicitar-acesso">Solicitar acesso</Link>
+                    </Menu.Item>
+                    <Menu.Item key="selecionar-modo" onClick={onMudarTheme} icon={iconTema}>{tema === 'light' ? 'Light' : 'Dark'}</Menu.Item>
                     <Menu.Item key="sair" icon={<LogoutOutlined/>} onClick={onSair}>Sair</Menu.Item>
                 </Menu.SubMenu>
             </Menu.Item>
