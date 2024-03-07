8a2a9ca8f00bb77d6a495e3004e56e62b2ede5a7
BrunaMarcelle <bruna.silva@vocedm.com.br>
2024-03-07 10:40:00 -0300

    [01] Implementado algumas funcoes de pesquisa e page de solicitar acesso e configuracoes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,16 @@
 import React,{FC, useState } from 'react';
-import { Link } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import { Menu, Input, Typography } from 'antd';
 import {LogoutOutlined, BulbOutlined, MoonOutlined} from '@ant-design/icons';
 import logo from '../assets/logo.svg';
 
 const HeaderApp: FC =() =>{
     const [tema, setTema] = useState<string> ('light');
+
+    //const navigate = useNavigate();
+
+    const usuario = 'Bruna Marcelle'
+    const iconTema = tema === 'light' ? <BulbOutlined /> : <MoonOutlined />;
 
     const onSair = () => (
         console.log("Você saiu xD")
@@ -17,25 +22,45 @@ const HeaderApp: FC =() =>{
         console.log('Você tentou mudar o theme da aplicação')
     };
 
-    const usuario = 'Bruna Marcelle'
-    const iconTema = tema === 'light' ? <BulbOutlined /> : <MoonOutlined />;
+    // const handleSearch = async (value: string) => {
+    //     try {
+    //         // Realizar a busca de documentos na API
+    //         const response = await fetch(`https://sua-api.com/documentos?termo=${value}`);
+    //         if (response.ok) {
+    //             const data = await response.json();
+    //             // Atualizar a interface do usuário com os documentos encontrados
+    //             console.log('Documentos encontrados:', data);
+    //         } else {
+    //             // Lidar com erros
+    //             console.error('Erro ao buscar documentos:', response.statusText);
+    //         }
+    //     } catch (error) {
+    //         console.error('Erro ao buscar documentos:', error);
+    //     }
+    // };
+
+    // const navigateToConfiguracoes = () => {
+    //     console.log("funciona o botão")
+    //     navigate('/configuracoes');
+    // };
+
+    // const navigateToSolicitarAcesso = () => {
+    //     navigate('/solicitar-acesso');
+    // };
+
     return (
         <Menu mode="horizontal" style={{ backgroundColor: tema === 'light' ? '#1628ca' : '#000', justifyContent: 'flex-start', alignItems: 'center', color: tema === 'light' ? '#000' : '#fff'}} >
             <Menu.Item key="logo">
                 <img src={logo} alt='Imagem do logo de Gerenciador de Documentos' style={{ paddingTop: '5px' }}/>
             </Menu.Item>
             <Menu.Item key="search" >
-                <Input.Search placeholder="Pesquisar..." style={{ paddingTop: '5px' }} />
+                <Input.Search placeholder="Pesquisar..." style={{ paddingTop: '5px' }}/>
             </Menu.Item>
             <Menu.Item  key="profile" style={{ marginLeft: 'auto' }}>
                 <Menu.SubMenu title={<span style={{ color: '#FFFFFF' }}>{'Perfil'}</span>} style={{ display: 'flex', justifyContent: 'center' }} >
                     <Typography.Text style={{ textAlign: 'center' }}>{usuario}</Typography.Text>
-                    <Menu.Item key="configuracoes">
-                        <Link to="/configuracoes">Configurações</Link>
-                    </Menu.Item>
-                    <Menu.Item key="solicitar-acesso">
-                        <Link to="/solicitar-acesso">Solicitar acesso</Link>
-                    </Menu.Item>
+                    <Menu.Item key="configuracoes">Configurações</Menu.Item>
+                    <Menu.Item key="solicitar-acesso">Solicitar acesso</Menu.Item>
                     <Menu.Item key="selecionar-modo" onClick={onMudarTheme} icon={iconTema}>{tema === 'light' ? 'Light' : 'Dark'}</Menu.Item>
                     <Menu.Item key="sair" icon={<LogoutOutlined/>} onClick={onSair}>Sair</Menu.Item>
                 </Menu.SubMenu>
